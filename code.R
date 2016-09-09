9e2892a25e10723c20a99ba6561957bb9bf485fb
janek <janek.thomas@web.de>
2016-09-09 17:35:10 +0200

    setups

--- code.R
+++ code.R
@@ -1,5 +1,5 @@
-load_all("~/rpackages/mlr/")
-load_all("~/rpackages/mlrMBO/")
+library(mlr)
+library(mlrMBO)
 library(OpenML)
 library(batchtools)
 
@@ -8,13 +8,20 @@ types = c("C-svc", "spoc-svc", "kbb-svc")
 
 OVERWRITE = TRUE
 
+if (OVERWRITE)
+  unlink("registry/", recursive = TRUE)
+
+reg$default.resources = list(walltime = 7200L, memory = 2000L, ntasks = 1L)
+
+reg = makeExperimentRegistry(packages = c("mlr", "mlrMBO", "OpenML"))
+
 
 lrns = lapply(types, function(type) {
   lrn = makeLearner("classif.ksvm", id = type, type = type)
   
   ps = makeParamSet(
-    makeNumericParam("C", -10, 10, trafo = function(x) 2^x),
-    makeNumericParam("sigma", -10, 10, trafo = function(x) 2^x))
+    makeNumericParam("C", -12, 12, trafo = function(x) 2^x),
+    makeNumericParam("sigma", -12, 12, trafo = function(x) 2^x))
   
   mbo.ctrl = makeMBOControl()
   
@@ -25,21 +32,13 @@ lrns = lapply(types, function(type) {
   ctrl = mlr:::makeTuneControlMBO(learner = surrogate.lrn,
     mbo.control = mbo.ctrl, same.resampling.instance = FALSE)
   
-  makeTuneWrapper(lrn, cv10, measures = acc, par.set = ps, control = ctrl)
+  makeTuneWrapper(lrn, cv5, measures = acc, par.set = ps, control = ctrl)
 })
+
 
 omls = listOMLDataSets(number.of.instances = c(100, 5000), number.of.features = c(5, 100), number.of.missing.values = 0L,
   number.of.classes = c(3, 10))
 
-
-unlink("registry/", recursive = TRUE)
-reg = makeExperimentRegistry()
-batchmark(learners = lrns, data.ids = omls$data.id[1:2], measures = acc, resamplings = cv2)
+batchmark(learners = lrns, data.ids = omls$data.id, measures = acc, resamplings = cv10)
 summarizeExperiments()
-submitJobs()
-getStatus()
-res = reduceBatchmarkResults(findDone())
-rmat = convertBMRToRankMatrix(res)
-plotBMRSummary(res)
-plotBMRBoxplots(res, acc, style = "violin")
 
